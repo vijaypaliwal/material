b975c8fc6dbf08c08dec7a8a016d78b46db71742
vijaypaliwal <vijay.paliwal@hotmail.com>
2015-10-29 15:12:16 +0530

    commit

--- plugins/org.apache.cordova.camera/www/CameraConstants.js
+++ plugins/org.apache.cordova.camera/www/CameraConstants.js
@@ -1,4 +1,4 @@
-/*
+cordova.define("org.apache.cordova.camera.Camera", function(require, exports, module) { /*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -51,3 +51,5 @@ module.exports = {
       FRONT: 1
   }
 };
+
+});
